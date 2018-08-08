28c882313fd4f90b660ecfe4f33c904f2f652515
leanip <leanip@qq.com>
2018-08-08 08:32:49 +0800

    54 新建购物车页面

---
+++ src/pages/Cart.vue
@@ -0,0 +1,31 @@
+<template lang='pug'>
+  .cart
+    van-nav-bar(title='购物车')
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        cartInfo: [],
+        isEmpty: false
+      }
+    },
+    created () {
+      this.getCartInfo()
+    },
+    methods: {
+      getCartInfo () {
+        if (localStorage.cartInfo) {
+          this.cartInfo = JSON.parse(localStorage.cartInfo)
+        }
+        console.log('cartInfo: ', this.cartInfo)
+        this.isEmpty = this.cartInfo.length ? true : false
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,35 +5,17 @@ import Register from '@/pages/Register'
 import Login from '@/pages/Login'
 import Goods from '@/pages/Goods'
 import CategoryList from '@/pages/CategoryList'
+import Cart from '@/pages/Cart'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'ShoppingMall',
-      component: ShoppingMall
-    },
-    {
-      path: '/register',
-      name: 'Register',
-      component: Register
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/goods',
-      name: 'Goods',
-      component: Goods
-    },
-    {
-      path: '/categorylist',
-      name: 'CategoryList',
-      component: CategoryList
-    }
+    { path: '/', name: 'ShoppingMall', component: ShoppingMall },
+    { path: '/register', name: 'Register', component: Register },
+    { path: '/login', name: 'Login', component: Login },
+    { path: '/goods', name: 'Goods', component: Goods },
+    { path: '/categorylist', name: 'CategoryList', component: CategoryList },
+    { path: '/cart', name: 'Cart', component: Cart}
   ]
 })
